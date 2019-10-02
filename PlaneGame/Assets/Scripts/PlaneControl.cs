139ecdc3f1e7ed6983978ebb3c371a65bbf8704e
jaxarthur <jaxarthur@outlook.com>
2019-10-02 14:50:13 -0600

    Progress

---
+++ PlaneGame/Assets/Scripts/PlaneControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneControl : MonoBehaviour
+{
+    //input vars
+    private float pitch;
+    private float roll;
+    private float fire0;
+    private float fire1;
+    private float fire2;
+    private float stickyThrottle;
+    private float floatingThrottle;
+    private float yaw;
+
+    //engine vars
+    public float maxEngineSpeed;
+    public float maxForce;
+
+    //control surface vars
+    public float maxPitchForce;
+    public float maxRollForce;
+    public float maxYawForce;
+
+    //wing vars
+    public float maxLiftForce;
+    public float liftForceRatio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetControl();
+
+        ApplyForce();
+    }
+
+    private void GetControl()
+    {
+        pitch = Input.GetAxis("Vertical");
+        roll = Input.GetAxis("Horizontal");
+        yaw = Input.GetAxis("Yaw");
+        fire0 = Input.GetAxis("Fire1");
+        fire1 = Input.GetAxis("Fire2");
+        fire2 = Input.GetAxis("Fire3");
+        stickyThrottle = Input.GetAxis("ThrottleSticky");
+        floatingThrottle = Input.GetAxis("ThrottleFloating");
+    }
+
+    private void ApplyForce()
+    {
+
+    }
+}
