dd117c7d96c9bbbf4204a6dddad8cdd96d9903ba
jaxarthur <jaxarthur@outlook.com>
2019-10-02 13:13:42 -0600

    Set up Controller

---
+++ PlaneGame/Assets/Scripts/InputTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTest : MonoBehaviour
+{
+    public float pitch;
+    public float roll;
+    public float fire0;
+    public float fire1;
+    public float fire2;
+    public float stickyThrottle;
+    public float floatingThrottle;
+    public float yaw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pitch = Input.GetAxis("Vertical");
+        roll = Input.GetAxis("Horizontal");
+        yaw = Input.GetAxis("Yaw");
+        fire0 = Input.GetAxis("Fire1");
+        fire1 = Input.GetAxis("Fire2");
+        fire2 = Input.GetAxis("Fire3");
+        stickyThrottle = Input.GetAxis("ThrottleSticky");
+        floatingThrottle = Input.GetAxis("ThrottleFloating");
+    }
+}
