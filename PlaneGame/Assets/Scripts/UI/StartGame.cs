28c6d949bcf90f8290fa47e2369666a185d4281b
jaxarthur <jaxarthur@outlook.com>
2019-11-01 15:01:02 -0600

    More UI stuff

--- PlaneGame/Assets/Scripts/UI/StartGame.cs
+++ PlaneGame/Assets/Scripts/UI/StartGame.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class StartGame
+public class StartGame : MonoBehaviour
 {
+    public Button startButton;
+    public Button hostButton;
+    public Button joinButton;
+
+    public GameObject NetworkManager;
+    public Component NetworkManagerScript;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startButton.onClick.AddListener(startGame);
+        hostButton.onClick.AddListener(hostGame);
+        joinButton.onClick.AddListener(goToJoinMenu);
+
+        NetworkManagerScript = NetworkManager.GetComponent<CustomNetworkManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void startGame()
+    {
+        NetworkManagerScript.StartHost();
+    }
+
+    void hostGame()
+    {
+
+    }
+
+    void goToJoinMenu()
+    {
+
     }
 }
