546122205e4a7d6c5375bc3bf51128e7c2b35863
jaxarthur <jaxarthur@outlook.com>
2019-12-17 17:14:51 -0700

    Player Input Godot

--- PlaneGameGodot/Player.gd
+++ PlaneGameGodot/Player.gd
@@ -1,57 +1,9 @@
-extends Spatial
-
-#Pitch Section
-var pitchIn: float = 0
-var pitchInUp: float = 0
-var pitchInDown: float = 0
-var pitchInAxis: float = 0
-var pitchInAxisLast: float = 0
-var pitchInUseAxis: bool = false
-
-#Roll Section
-var rollIn: float = 0
-var rollInUp: float = 0
-var rollInDown: float = 0
-var rollInAxis: float = 0
-var rollInAxisLast: float = 0
-var rollInUseAxis: bool = false
-
-#Yaw Section
-var yawIn: float = 0
-var yawInUp: float = 0
-var yawInDown: float = 0
-var yawInAxis: float = 0
-var yawInAxisLast: float = 0
-var yawInUseAxis: bool = false
-
-#Throttle Section
-var throttleIn: float = 0
-var throttleInUp: float = 0
-var throttleInDown: float = 0
-var throttleInSticky: float = 0
-var throttleInAxis: float = 0
-var throttleInAxisLast: float = 0
-var throttleInUseAxis: bool = false
+extends RigidBody
+const InputManagerScript = preload("res://InputManager.gd")
+var inputManager: InputManagerScript
 
 func _ready():
-	pass
-	
-func _process(delta):
-	if (self.is_network_master()):
-		
-		#Pitch Section
-		pass
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	inputManager = find_node("InputManager")
+
+func _integrate_forces(state):
+	inputManager.update_input()
